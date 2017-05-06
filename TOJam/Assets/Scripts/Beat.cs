c38d6642a058c68ffb96a7e8cd58d1aa96931496
andusy <andus401@gmail.com>
2017-05-05 20:07:33 -0400

    sample music bar

---
+++ TOJam/Assets/Scripts/Beat.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beat : MonoBehaviour {
+
+	private Rigidbody2D rbdy;
+    public Collider2D beat_Collider;
+
+    private float speed;
+    private int bpm;
+
+    private Vector2 loc;
+    private Quaternion rot;
+
+    // Use this for initialization
+    void Start () {
+		rbdy = GetComponent<Rigidbody2D> ();
+        beat_Collider = GetComponent<Collider2D>();
+
+        bpm = 130;
+        speed = bpm / 30f;
+
+        loc = new Vector2(10.5f, -3.35f);
+        rot = new Quaternion(0, 0, 0, 0);
+
+        InvokeRepeating("Instantiate", 0.5f, 0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+            
+		float hor = 0 - speed;
+        float vert = 0;
+		Vector2 beat = new Vector2 (hor,vert);
+        rbdy.velocity = beat * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+    private void Instantiate()
+    {
+        Instantiate(gameObject, loc, rot);
+    }
+}
---
+++ TOJam/Assets/Scripts/Music_Bar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_Bar : MonoBehaviour {
+
+    public Collider2D bar_Collider;
+
+	// Use this for initialization
+	void Start () {
+        bar_Collider = GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+}
